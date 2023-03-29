1a6321d908ee9ca59801f6cc15622432a605329b
JustWaveThings <justwavethings@proton.me>
2023-03-29 17:26:14 -0400

    shipFactory Tests and functions written to pass those tests

---
+++ src/shipFactory.js
@@ -0,0 +1,32 @@
+/*
+Your ‘ships’ will be objects that include their length, the number of times they’ve been hit and whether or not they’ve been sunk.
+*/
+function shipFactory(length) {
+	const ship = {
+		length,
+		hits: 0,
+		hit() {
+			this.hits += 1;
+		},
+		isSunk() {
+			return this.hits === this.length;
+		},
+	};
+	const publicShip = {
+		get length() {
+			return ship.length;
+		},
+		get isSunk() {
+			return ship.isSunk();
+		},
+		hit() {
+			ship.hit();
+		},
+		get numberOfHits() {
+			return ship.hits;
+		},
+	};
+	return publicShip;
+}
+
+export default shipFactory;
---
+++ src/shipFactory.test.js
@@ -0,0 +1,47 @@
+import shipFactory from './shipFactory';
+
+/*
+tests  for  shipFactory function
+
+test only public interface -- try to follow sandi metz's rules
+
+Your ‘ships’ will be objects that include their length, the number of times they’ve been hit and whether or not they’ve been sunk.
+
+
+what are the public interface methods of shipFactory?
+- length
+- hit
+- isSunk
+*/
+
+describe('shipFactory', () => {
+	describe('length', () => {
+		it('should return the length of the ship', () => {
+			const ship = shipFactory(5);
+			expect(ship.length).toBe(5);
+		});
+	});
+
+	describe('hit', () => {
+		it('should increment the number of hits', () => {
+			const ship = shipFactory(5);
+			ship.hit();
+			expect(ship.numberOfHits).toBe(1);
+		});
+	});
+
+	describe('isSunk', () => {
+		it('should return false if the ship has not been sunk', () => {
+			const ship = shipFactory(5);
+			expect(ship.isSunk).toBe(false);
+		});
+
+		it('should return true if the ship has been sunk', () => {
+			const ship = shipFactory(5);
+			for (let i = 0; i < 5; i++) {
+				ship.hit();
+			}
+			expect(ship.isSunk).toBe(true);
+		});
+	});
+});
