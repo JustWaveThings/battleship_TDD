9b5fed42916bea4a03384faf2028fcd567377e26
JustWaveThings <justwavethings@proton.me>
2023-03-31 16:11:21 -0400

    computerMove returns valid move - PlayerFactory done.

---
+++ src/player.js
@@ -0,0 +1,44 @@
+import GameboardFactory from './gameboard';
+
+let gameboard = GameboardFactory();
+let movesList = [];
+
+function PlayerFactory(name) {
+	function getPlayerName() {
+		return name;
+	}
+
+	function playerMove(x, y) {
+		return [x, y];
+	}
+
+	function isCurrentPlayer(turn) {
+		turn++;
+		return turn % 2 === 0 ? true : false;
+	}
+
+	function computerMove() {
+		const randomMove = Math.floor(
+			Math.random() * gameboard.gameboard.flat().length
+		);
+		let move = gameboard.gameboard.flat()[randomMove];
+
+		if (move[2] === 'empty' && movesList.includes(move) === false) {
+			movesList.push(move);
+		} else if (move[2] === 'ship' && movesList.includes(move) === false) {
+			movesList.push(move);
+		} else {
+			computerMove();
+		}
+
+		return `[${move[0]}, ${move[1]}]`;
+	}
+
+	return {
+		getPlayerName,
+		playerMove,
+		isCurrentPlayer,
+	};
+}
+
+export default PlayerFactory;
---
+++ src/player.test.js
@@ -0,0 +1,34 @@
+import PlayerFactory from './player';
+/*
+Write tests for playerFactory
+
+what are the public methods and properties of playerFactory?
+
+- set / get PlayerName
+- playerMove
+- isCurrentPlayer
+
+*/
+
+describe('PlayerFactory', () => {
+	let player;
+	let turn = 0;
+	let playX = 0;
+	let playY = 0;
+
+	beforeEach(() => {
+		player = PlayerFactory('Jimmy');
+	});
+
+	test('setPlayerName', () => {
+		expect(player.getPlayerName()).toEqual('Jimmy');
+	});
+
+	test('playerMove - expect to send', () => {
+		expect(player.playerMove(playX, playY)).toEqual([0, 0]);
+	});
+
+	test('isCurrentPlayer', () => {
+		expect(player.isCurrentPlayer(turn)).toEqual(false);
+	});
+});
