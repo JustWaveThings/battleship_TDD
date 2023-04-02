da4fc3ec333744f7bb99eefa5ee77a79ecf086a6
JustWaveThings <justwavethings@proton.me>
2023-04-02 11:03:34 -0400

    testing different UI approach

--- src/gameboard.js
+++ src/gameboard.js
@@ -11,7 +11,6 @@ function GameboardFactory() {
 	);
 
 	let missedAttacksList = [];
-	let ships = [];
 
 	gameboard.map(row => {
 		row.map(space => {
@@ -29,21 +28,11 @@ function GameboardFactory() {
 			y
 		);
 		tentShipCoords.forEach(space => {
-			if (
-				space[2] === 'empty' ||
-				space[2] === 'buffer' ||
-				space[2] === 'ship'
-			) {
+			if (space[2] === 'empty') {
 				space[2] = 'ship';
 			} else {
-				console.log(space[2], 'space[2]');
 				throw new Error('Invalid ship placement on board');
 			}
-		});
-		ships.push({
-			ship,
-			position: bowPosition,
-			orientation: axis,
 		});
 		shipGiveOneSpaceBuffer();
 	}
@@ -111,13 +100,14 @@ function GameboardFactory() {
 
 	function receiveAttack(position) {
 		const [x, y] = position;
-
 		const space = gameboard[x][y];
-		if (space[2] === 'ship' || space[2] === 'hit') {
+		if (space[2] === 'ship') {
 			space[2] = 'hit';
+		} else if (space[2] === 'miss') {
+			throw new Error('You already attacked this space');
 		} else {
 			space[2] = 'miss';
-			// trackMissedAttacks(space);
+			trackMissedAttacks(space);
 		}
 	}
 
@@ -134,17 +124,12 @@ function GameboardFactory() {
 		return areAllShipsSunk;
 	}
 
-	function getShips() {
-		return ships;
-	}
-
 	return {
 		gameboard,
 		placeShip,
 		receiveAttack,
 		allShipsSunk,
 		missedAttacksList,
-		getShips,
 	};
 }
 
--- src/player.js
+++ src/player.js
@@ -1,5 +1,9 @@
-function PlayerFactory(name, gameboard) {
-	let computerMovesList = [];
+import GameboardFactory from './gameboard';
+
+let gameboard = GameboardFactory();
+let movesList = [];
+
+function PlayerFactory(name) {
 	function getPlayerName() {
 		return name;
 	}
@@ -7,47 +11,33 @@ function PlayerFactory(name, gameboard) {
 	function playerMove(x, y) {
 		return [x, y];
 	}
-	function playerAttack(move) {
-		gameboard.receiveAttack(move);
-	}
 
 	function isCurrentPlayer(turn) {
 		turn++;
 		return turn % 2 === 0 ? true : false;
 	}
 
-	function computerAttack() {
-		const move = computerMove();
-		const [x, y] = move;
-		gameboard.receiveAttack(move);
-		return move;
-	}
 	function computerMove() {
 		const randomMove = Math.floor(
 			Math.random() * gameboard.gameboard.flat().length
 		);
 		let move = gameboard.gameboard.flat()[randomMove];
 
-		if (
-			move[2] === 'empty' ||
-			move[2] === 'buffer' ||
-			(move[2] === 'ship' && computerMovesList.includes(move) === false)
-		) {
-			computerMovesList.push(move);
-			console.log(computerMovesList, 'computerMovesList');
+		if (move[2] === 'empty' && movesList.includes(move) === false) {
+			movesList.push(move);
+		} else if (move[2] === 'ship' && movesList.includes(move) === false) {
+			movesList.push(move);
 		} else {
-			return computerMove();
+			computerMove();
 		}
 
-		return [move[0], move[1]];
+		return `[${move[0]}, ${move[1]}]`;
 	}
 
 	return {
 		getPlayerName,
 		playerMove,
 		isCurrentPlayer,
-		computerAttack,
-		playerAttack,
 	};
 }
 
